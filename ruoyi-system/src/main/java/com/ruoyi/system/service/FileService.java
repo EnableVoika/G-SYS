bf374060548a56ca403207903c138d326f8901b6
Draco Gore <longmietian@gmail.com>
2025-06-18 18:23:15 +0800

    修改回收站和数据表逻辑

--- ruoyi-system/src/main/java/com/ruoyi/system/service/FileService.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/FileService.java
@@ -2,11 +2,9 @@ package com.ruoyi.system.service;
 
 import com.ruoyi.common.core.domain.DelFailFile;
 import com.ruoyi.common.core.domain.FileVO;
-import com.ruoyi.common.core.domain.dto.FileDTO;
 import com.ruoyi.common.core.domain.dto.RecycleListDTO;
 import com.ruoyi.common.core.domain.vo.system.filecontroller.RecycleVO;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
